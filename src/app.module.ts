600376027859e127f50005f3357b2048eabaf47e
darwin1224 <darwinblacks@gmail.com>
2019-10-30 22:40:36 +0700

    Add mongooseModule forRoot in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ArticleModule } from './article/article.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://database/nest_mongo', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    ArticleModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
