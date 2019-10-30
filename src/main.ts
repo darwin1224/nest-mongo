41395f31dc6c8b76e6b13e7b9961bb4504a16ded
darwin1224 <darwinblacks@gmail.com>
2019-10-30 22:39:59 +0700

    Add validation pipe and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://frontend',
+    allowedHeaders: ['GET', 'POST', 'PUT', 'PATCH', 'HEAD', 'OPTIONS'],
+  });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
